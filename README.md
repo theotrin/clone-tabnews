5d1c4810f4b4ea5936e7daef2b3b0e17a156e718
Theo <102327842+TheoTrin@users.noreply.github.com>
2023-06-23 20:50:37 +0000

    prettier adcionado e devidamente configurado, com scripts lint:checke lint:fix para a correção de arquivos

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 implementação do tabnews para o curso do Felipe Deschamps!
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
-function Home(){
-    return <h1> oi, te amo :DD </h1>
+function Home() {
+  return <h1> oi, te amo :DD </h1>;
+}
+function teste() {
+  console.log("1,2,3 testando");
+}
+
+function teste2() {
+  console.log("sera que você vai consertar?");
 }
 
 export default Home;
