7feb5345c33ee2cc84ab294780714454ef02ba05
unknown <theopinheiro12@gmail.com>
2023-12-18 16:12:55 -0400

    testes obsolentos removidos

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,5 @@
 function Home() {
   return <h1> Olá, vamos codar!</h1>;
 }
-function teste() {
-  console.log("1,2,3 testando");
-}
-
-function teste2() {
-  console.log("sera que você vai consertar?");
-}
 
 export default Home;
