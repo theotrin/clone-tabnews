78d2820ec9311168ee50ad4c20c7ea935a3fec83
unknown <theopinheiro12@gmail.com>
2024-03-22 14:45:34 -0400

    erros corrigidos

--- infra/database.js
+++ infra/database.js
@@ -2,10 +2,10 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    host: process.env.POSTRGRES_HOST,
-    port: process.env.POSTRGRES_PORT,
-    user: process.env.POSTRGRES_USER,
-    database: process.env.POSTRGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
