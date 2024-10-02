43e9461d2e4683027a9dd86919a827b8af5c3872
theotrin <theopinheiro12@gmail.com>
2024-10-02 15:08:54 -0400

    handle database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,3 @@
-
 import { Client } from "pg";
 
 async function query(queryObject) {
@@ -8,7 +7,9 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
+
   console.log("Credenciais do Postgres:", {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
